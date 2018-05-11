e462723607d68ce958c40a4031415c5dd0894206
tabby42 <irene.theiss@gmx.at>
2018-05-11 17:22:57 +0200

    add partial for frontpage boxes

---
+++ resources/views/partials/frontpage-boxes.blade.php
@@ -0,0 +1,17 @@
+<div class="row">
+  <div class="col-md">
+    <figure >
+      <a href="#" title="die clownin"><figcaption><h2>die clownin</h2></figcaption></a>
+      <img src="@asset('images/konzaerd_schrei.jpg')" alt="clownin">
+    </figure>
+  </div>
+  <div class="col-md">
+    <div class="col col-spacer d-sm-block">
+
+    </div>
+    <figure >
+      <a href="#" title="die lehrerin"><figcaption><h2>die lehrerin</h2></figcaption></a>
+      <img src="@asset('images/workshop_fuesse.jpg')" alt="lehrerin">
+    </figure>
+  </div>
+</div>
