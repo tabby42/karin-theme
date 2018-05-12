b0b78b3db0965601018680fb191041a76c695c61
tabby42 <irene.theiss@gmx.at>
2018-05-12 12:33:53 +0200

    add correct href to links

--- resources/views/partials/frontpage-boxes.blade.php
+++ resources/views/partials/frontpage-boxes.blade.php
@@ -1,7 +1,7 @@
-<div class="row">
+<div class="row boxed-row">
   <div class="col-md">
     <figure >
-      <a href="#" title="die clownin"><figcaption><h2>die clownin</h2></figcaption></a>
+      <a href="<?php echo get_site_url(null, '/die-clownin'); ?>" title="die clownin"><figcaption><h2>die clownin</h2></figcaption></a>
       <img src="@asset('images/konzaerd_schrei.jpg')" alt="clownin">
     </figure>
   </div>
@@ -10,7 +10,7 @@
 
     </div>
     <figure >
-      <a href="#" title="die lehrerin"><figcaption><h2>die lehrerin</h2></figcaption></a>
+      <a href="<?php echo get_site_url(null, '/die-lehrerin'); ?>" title="die lehrerin"><figcaption><h2>die lehrerin</h2></figcaption></a>
       <img src="@asset('images/workshop_fuesse.jpg')" alt="lehrerin">
     </figure>
   </div>
