65ac40695e06294d3ff38c0d13f7ee456367530b
tabby42 <irene.theiss@gmx.at>
2018-07-01 15:21:31 +0200

    add js for toggling event details

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,6 +1,13 @@
 export default {
   init() {
     // JavaScript to be fired on all pages
+    var eventSummary = $("article.event .entry-summary"),
+        btnDetail = $(".btn.btn-details");
+
+    eventSummary.hide();
+    btnDetail.on("click", function() {
+      $(this).parent().parent().find(".entry-summary").slideToggle(1000);
+    });
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
