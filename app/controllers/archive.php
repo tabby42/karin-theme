3b91f54339e5bef078d8e0d7d39f59c9d6eef8d1
tabby42 <irene.theiss@gmx.at>
2018-07-01 13:40:55 +0200

    adjust eventloop method

--- app/controllers/archive.php
+++ app/controllers/archive.php
@@ -16,9 +16,11 @@ class Archive extends Controller
         return array_map(function ($post) {
             return [
                 'content' => apply_filters('the_content', $post->post_content),
+                'title' => apply_filters('the_content', $post->post_title),
                 'event_date' =>  date_format(date_create(get_post_meta($post->ID, 'event_date', true )), 'd.m.Y'),
                 'event_time' => get_post_meta($post->ID, 'event_time', true ),
                 'location' => get_post_meta($post->ID, 'location', true ),
+                'address' => get_post_meta($post->ID, 'event_address', true ),
                 'is_workshop' => get_post_meta($post->ID, 'is_workshop', true ),
             ];
         }, $event_items);
