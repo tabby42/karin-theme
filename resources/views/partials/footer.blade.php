1820fd7a7b45bda475d38c5cf7508bbb39f030a7
tabby42 <irene.theiss@gmx.at>
2018-07-30 10:46:54 +0200

    modify footer markup

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -11,18 +11,19 @@
   </div>
   <div class="row">
     <div class="col-md-3 footer-col">
-      <a href="#" class="nav-link footer-link">Impressum</a>
+      <a href="<?php echo get_site_url(null, '/impressum'); ?>" class="nav-link footer-link">Impressum</a>
     </div>  
     <div class="col-md-3">
 
     </div>
     <div class="col-md-3">
     </div>
-    <div class="col-md-3">
+    <div class="col-md-3 footer-col footer-flex">
       <img src="@asset('images/karin_logo-inverse.svg')" alt="Logo">
       <p>
-        <span>post@karin-theiss.com</span><br>
-        <span>+43 699 11 55 36 82</span>
+        <span><em>T&emsp;</em>  post@karin-theiss.com</span><br>
+        <span><em>E&emsp;</em>  +43 699 11 55 36 82</span><br>
+        <span><a class="footer-link position-st" href="https://www.facebook.com/profile.php?id=100013285934303" target="_blank"><em>F&emsp;</em>  die clownin auf facebook</a></span>
       </p>
       <button type="button" class="btn btn-default" data-toggle="modal" data-target="#kt-mail-modal">nachricht</button>
     </div>
